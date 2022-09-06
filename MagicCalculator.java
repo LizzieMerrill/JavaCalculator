42ac1d059e0195eda922ed3b0c4d91fd6896cc2a
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-09-05 17:23:38 -0700

    Update MagicCalculator.java
    Finishing touches

--- MagicCalculator.java
+++ MagicCalculator.java
@@ -1,3 +1,23 @@
-public class MagicCalculator extends Calculator{
-
-}
+import java.lang.Math;
+//more functional extension of Calculator class
+public class MagicCalculator extends Calculator {
+	public static double squareRoot(double num) {
+		return Math.sqrt(num);
+	}
+	public static double sin(double num) {
+		return Math.sin(num);
+	}
+	public static double cosine(double num) {
+		return Math.cos(num);
+	}
+	public static double tangent(double num) {
+		return Math.tan(num);
+	}
+	public static int factorial(int num) {
+		int factorial = 1;
+		for (int i = 1; i < num; ++i) {
+			factorial = factorial*i;
+		}
+		return factorial;
+	}
+}//end of class
