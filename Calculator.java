5b30595f5899317247b31861a55aa8a97f809932
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-09-05 17:11:55 -0700

    Update Calculator.java
    Initial work

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,18 @@
-class Calculator{
-
-}
+//parent class of MagicCalculator, has 5 basic functions
+public class Calculator {
+	public static int add(int num1, int num2) {
+		return num1+num2;
+	}
+	public static int subtract(int num1, int num2) {
+		return num1-num2;
+	}
+	public static int multiply(int num1, int num2) {
+		return num1*num2;
+	}
+	public static double divide(int num1, int num2) {
+		return (double)num1/(double)num2;
+	}
+	public static int square(int num1) {
+		return num1*num1;
+	}
+}//end of class
